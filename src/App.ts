e8fc5ac5c7788cea2713651e55e829fc3bd1184d
fabio abrantes diniz <fabio.abrantes.diniz@gmail.com>
2020-06-07 20:39:23 -0300

    exemplo de typescript no node

---
+++ src/App.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import routes from './routes'
+
+class App {
+  public express: express.Application
+
+  public constructor () {
+    this.express = express()
+    this.middlewares()
+    this.database()
+    this.routes()
+  }
+
+  private middlewares ():void{
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
+
+  private database (): void{
+    mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', { useNewUrlParser: true })
+  }
+
+  private routes ():void{
+    this.express.use(routes)
+  }
+}
+export default new App().express
---
+++ src/controllers/UserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import User from '../schemas/User'
+
+class UserController {
+  public async index (request:Request, response:Response):Promise<Response> {
+    const users = await User.find()
+    return response.json(users)
+  }
+
+  public async store (request:Request, response:Response):Promise<Response> {
+    const user = await User.create(request.body)
+
+    return response.json(user)
+  }
+}
+export default new UserController()
